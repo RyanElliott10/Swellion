89da9b120db77dbdd83307ea086be0f1c31b3c07
Ryan Elliott <ryan_m_elliott@apple.com>
2022-07-26 21:22:38 -0700

    Add inital response structs

---
+++ src/api/common.rs
@@ -0,0 +1,15 @@
+pub enum HeightUnits {
+    Feet,
+    Meters,
+}
+
+pub enum TemperatureUnits {
+    Fahrenheit,
+    Celsius,
+}
+
+pub enum SpeedUnits {
+    Knots,
+    KilometersPerHour,
+    MilesPerHour,
+}
